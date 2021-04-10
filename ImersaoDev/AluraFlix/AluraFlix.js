4fca94998ad1bfaa6518d59c6becf18f3f9ee8bd
MatthewsTomts <71537027+MatthewsTomts@users.noreply.github.com>
2021-04-09 22:13:48 -0300

    AluraFlix Desafio
    Exibir filme escolhido pelo usuario e não mostrar o mesmo filme duas vezes seguidas

--- ImersaoDev/AluraFlix/AluraFlix.js
+++ ImersaoDev/AluraFlix/AluraFlix.js
@@ -4,7 +4,25 @@ var nomeFilmes = ["Matrix", "One Piece", "Os Sem Floresta", "Scott Pilgrim Vs. T
 
 var url = ["https://www.imdb.com/title/tt0133093/?ref_=fn_al_tt_1", "https://www.imdb.com/title/tt0388629/?ref_=fn_al_tt_1", "https://www.imdb.com/title/tt0327084/?ref_=fn_al_tt_1", "https://www.imdb.com/title/tt0446029/?ref_=fn_al_tt_1"] 
 
-for (var i = 0; i < listaFilmes.length; i++) {
+var filmeExibido = 7
+
+function exibirFilme(){
+  var filme = parseInt(prompt("Qual Filme você quer? \n1-Matrix \n2-One Piece \n3-Os Sem Floresta \n4-Scott Pilgrim"))
+
+  var teste = document.getElementById("teste")
+  if(filmeExibido != filme){
+    if(filme > 0 && filme < 5){
+      teste.innerHTML = "<img src=" + listaFilmes[filme-1] + "><h2><a href='" + url[filme-1] + "' target='_blank'>" + nomeFilmes[filme-1] + "</a></h2>"
+    } else {
+      teste.innerHTML = "<h2>Digite um valor válido</h2>"
+    }
+  } else {
+    teste.innerHTML = "<h2>Você já viu esse filme!</h2>"
+  }
+  filmeExibido = filme
+}
+
+/*for (var i = 0; i < listaFilmes.length; i++) {
   document.write("<img src=" + listaFilmes[i] + ">")
   document.write("<h2><a href='" + url[i] + "' target='_blank'>"+ nomeFilmes[i] + "</a></h2>")
-}
+}*/
